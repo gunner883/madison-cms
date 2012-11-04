fc26ae98b25c1ba6b0b3c42181851bdbf64c818c
rlombardo <rocco.lombardo@northps.com>
2012-11-04 12:30:30 -0500

    initial commit

---
+++ app_data/bd2ea6b0-4c1d-4051-8bdf-85c1dd68eeb2/NewStandardValueKeyTreeAction.js
@@ -0,0 +1,3 @@
+﻿function actionNewStandardValueKey() {
+    UmbClientMgr.openModalWindow('plugins/Sitereactor/StandardValues/EditStandardValueKeys.aspx?id=' + UmbClientMgr.mainTree().getActionNode().nodeId, 'Create New Key', true, 400, 400);
+}
